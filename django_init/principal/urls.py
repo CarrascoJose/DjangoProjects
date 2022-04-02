8bfc121ebe5c1278c80340780f7374b33134e9db
CarrascoJose <jmcotema@gmail.com>
2022-04-02 13:51:38 -0300

    Vistas basadas en funciones

--- django_init/principal/urls.py
+++ django_init/principal/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio')
+    path('inicio/',views.start,name="index"),
+    path('agregar_persona',views.crearPersona,name="crear"),
+    path('editar_persona/<int:id>',views.editarPersona,name="editar"),
+    path('eliminar_persona/<int:id>',views.eliminarPersona,name="eliminar")
 ]
--- django_init/principal/views.py
+++ django_init/principal/views.py
@@ -1,6 +1,51 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
 from .models import Persona
+from .forms import PersonaForm
 # Create your views here.
 
 def start(request):
-    return render(request,'index.html')
+    personas = Persona.objects.all() #select * from Persona
+
+    contexto = {
+        "personas":personas,
+    }
+    return render(request,'principal/index.html',contexto)
+    
+def crearPersona(request):
+    if request.method == "GET":
+        form = PersonaForm()
+        contexto = {
+            'form':form
+        }
+    else:
+        form = PersonaForm(request.POST)
+        contexto = {
+            'form':form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request,'principal/crear_persona.html',contexto)
+
+def editarPersona(request,id):
+    persona = Persona.objects.get(id=id)
+    if request.method=="GET":
+        form = PersonaForm(instance=persona)
+        contexto = {'form':form}
+    
+    else:
+        form = PersonaForm(request.POST, instance=persona)
+        contexto = {
+            'form':form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    return render(request, 'principal/crear_persona.html',contexto)
+
+def eliminarPersona(request,id):
+    persona = Persona.objects.get(id=id)
+    persona.delete()
+    return redirect('index')
